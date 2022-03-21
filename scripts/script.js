b78dabefe2a2a0c4f27fcef2708ebabc03fd13c6
Esenia Voinova <esenia.voinova@yandex.ru>
2022-03-21 17:03:54 +0500

    fix: исправить JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,27 @@
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let exitButton = popup.querySelector('.popup__exit');
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.profile__name');
+let jobInput = document.querySelector('.profile__about');
+let name = formElement.querySelector('.popup__input_data_name');        
+let description = formElement.querySelector('.popup__input_data_job');
+
 function openPopup() {
     popup.classList.add('popup_opened');
+    nameInput.textContent = 'Жак-Ив Кусто';
+    jobInput.textContent = 'Исследователь океана';
 };
 function closePopup() {
     popup.classList.remove('popup_opened');
 };
-editButton.addEventListener('click', openPopup);
-exitButton.addEventListener('click', closePopup);
-
-
-let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.profile__name');
-let jobInput = document.querySelector('.profile__about');
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-
-    let name = formElement.querySelector('.popup__input_data_name');        
-    let description = formElement.querySelector('.popup__input_data_job');
-
     nameInput.textContent = name.value;
     jobInput.textContent = description.value;
+    closePopup();
 };
+editButton.addEventListener('click', openPopup);
+exitButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
