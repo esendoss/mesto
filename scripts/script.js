37d09d2ff7221f5242bdd1595a19720f20f84009
Esenia Voinova <esenia.voinova@yandex.ru>
2022-04-01 20:28:53 +0500

    feat: добавить попап карточек

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,60 @@
-let popup = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
-let exitButton = popup.querySelector('.popup__exit');
-let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.profile__name');
-let jobInput = document.querySelector('.profile__about');
-let name = formElement.querySelector('.popup__input_data_name');        
-let description = formElement.querySelector('.popup__input_data_job');
+//переменная попапа редактирования профиля
+const editPopup = document.querySelector('.popup_edit');
+//переменная для кнопки редактирования профиля
+const editButton = document.querySelector('.profile__edit-button');
+//переменная для кнопки выхода из редактирования профиля
+const exitButton = document.querySelector('.popup__exit');
 
-function openPopup() {
-    popup.classList.add('popup_opened');
+const formElement = document.querySelector('.popup__container');
+//переменные данных в редакторе профиля
+const nameInput = document.querySelector('.profile__name');
+const jobInput = document.querySelector('.profile__about');
+//переменные инпутов в редакторе профиля
+const name = formElement.querySelector('.popup__input_data_name');        
+const description = formElement.querySelector('.popup__input_data_job');
+
+/* создадим переменные для кнопки добавления и для инпутов 
+в попапе добавления изображения */
+const addPopup = document.querySelector('.popup_add');
+const addButton = document.querySelector('.profile__add-button');
+const titleInput = document.querySelector ('.gallery__place');
+const imgInput = document.querySelector ('.gallery__img');
+const title = formElement.querySelector('.popup__input_data_title');
+const img = formElement.querySelector('.popup__input_data_img');
+
+
+//функция открытия попапа кнопки правки
+function openEditPopup() {
+  editPopup.classList.add('popup_opened');
     name.value = nameInput.textContent;
     description.value = jobInput.textContent;
 };
-function closePopup() {
-    popup.classList.remove('popup_opened');
+//Закрытие попапа кнопки правки
+function closeEditPopup() {
+  editPopup.classList.remove('popup_opened');
 };
-
+//Функция сохранения данных профиля
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     nameInput.textContent = name.value;
     jobInput.textContent = description.value;
-    closePopup();
+    closeEditPopup();
 };
-editButton.addEventListener('click', openPopup);
-exitButton.addEventListener('click', closePopup);
-formElement.addEventListener('submit', formSubmitHandler);
+//слушатели для открытия и закрытия попапа редактора профиля
+editButton.addEventListener('click', openEditPopup);
+exitButton.addEventListener('click', closeEditPopup);
+//слушатель для кнопки сохранения
+formElement.addEventListener('submit', closeEditPopup);
+
+function openAddPopup() {
+  addPopup.classList.add('popup_opened-add');
+};
+function closeAddPopup() {
+  addPopup.classList.remove('popup_opened-add');
+};
+// Открытие/закрытие попапа кнопки добавления 
+addButton.addEventListener('click', openAddPopup);
+addButton.addEventListener('click', closeAddPopup); 
 
 /* Добавит 6 карточек */
 const initialCards = [
@@ -53,14 +83,3 @@ const initialCards = [
       link: 'https://unsplash.com/photos/XyjIUYxJrGo'
     }
   ];
-
-/* создадим переменные для кнопки добавления и для инпутов 
-в попапе добавления изображения */
-let addButton = document.querySelector('.profile__add-button');
-let title = formElement.querySelector('.popup__input_data_title');
-let img = formElement.querySelector('.popup__input_data_img');
-
-/* Откртие/закртие попапа кнопки добавления */
-
-addButton.addEventListener('click', openPopup);
-addButton.addEventListener('click', closePopup);
