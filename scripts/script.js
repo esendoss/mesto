ba097353753987649b51489c51ba975159629db6
Esenia Voinova <esenia.voinova@yandex.ru>
2022-04-03 21:27:45 +0500

    fix: переделать код для попапов редактирования профиля и добалвения фото

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,12 @@
-//переменная попапа редактирования профиля
-const editPopup = document.querySelector('.popup_edit');
-//переменная для кнопки редактирования профиля
+//переменные для попапов
 const editButton = document.querySelector('.profile__edit-button');
-//переменная для кнопки выхода из редактирования профиля
-const exitButton = document.querySelector('.popup__exit');
+const addButton = document.querySelector('.profile__add-button');
+const editPopup = document.querySelector('.popup_edit');
+const addPopup = document.querySelector('.popup_add');
+
+//переменные для кнопки выхода 
+const exitEditButton = document.querySelector('.popup__exit_edit');
+const exitAddButton = document.querySelector('.popup__exit_add');
 
 const formElement = document.querySelector('.popup__container');
 //переменные данных в редакторе профиля
@@ -13,73 +16,27 @@ const jobInput = document.querySelector('.profile__about');
 const name = formElement.querySelector('.popup__input_data_name');        
 const description = formElement.querySelector('.popup__input_data_job');
 
-/* создадим переменные для кнопки добавления и для инпутов 
-в попапе добавления изображения */
-const addPopup = document.querySelector('.popup_add');
-const addButton = document.querySelector('.profile__add-button');
-const titleInput = document.querySelector ('.gallery__place');
-const imgInput = document.querySelector ('.gallery__img');
-const title = formElement.querySelector('.popup__input_data_title');
-const img = formElement.querySelector('.popup__input_data_img');
-
-
-//функция открытия попапа кнопки правки
-function openEditPopup() {
-  editPopup.classList.add('popup_opened');
-    name.value = nameInput.textContent;
-    description.value = jobInput.textContent;
-};
-//Закрытие попапа кнопки правки
-function closeEditPopup() {
+//функции открытия попапов
+editButton.addEventListener('click', () => {
+  editPopup.classList.toggle('popup_opened');
+  name.value = nameInput.textContent;
+  description.value = jobInput.textContent;
+});
+exitEditButton.addEventListener('click', () => {
   editPopup.classList.remove('popup_opened');
-};
+});
+addButton.addEventListener('click', () => {
+  addPopup.classList.toggle('popup_opened');
+});
+exitAddButton.addEventListener('click', () => {
+  addPopup.classList.remove('popup_opened');
+});
+
 //Функция сохранения данных профиля
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     nameInput.textContent = name.value;
     jobInput.textContent = description.value;
-    closeEditPopup();
+    editPopup.classList.remove('popup_opened');
 };
-//слушатели для открытия и закрытия попапа редактора профиля
-editButton.addEventListener('click', openEditPopup);
-exitButton.addEventListener('click', closeEditPopup);
-//слушатель для кнопки сохранения
-formElement.addEventListener('submit', closeEditPopup);
-
-function openAddPopup() {
-  addPopup.classList.add('popup_opened-add');
-};
-function closeAddPopup() {
-  addPopup.classList.remove('popup_opened-add');
-};
-// Открытие/закрытие попапа кнопки добавления 
-addButton.addEventListener('click', openAddPopup);
-addButton.addEventListener('click', closeAddPopup); 
-
-/* Добавит 6 карточек */
-const initialCards = [
-    {
-      name: 'Алтай',
-      link: 'https://unsplash.com/photos/k1QPhErFvMw'
-    },
-    {
-      name: 'Уральские горы',
-      link: 'https://unsplash.com/photos/vm4Onc3qVAY'
-    },
-    {
-      name: 'Пик Звёздный',
-      link: 'https://unsplash.com/photos/GBa0kiHMhEM'
-    },
-    {
-      name: 'Исландия',
-      link: 'https://unsplash.com/photos/e9QfTnB647Y'
-    },
-    {
-      name: 'Кейп Таун',
-      link: 'https://unsplash.com/photos/joG5Wt96qBM'
-    },
-    {
-      name: 'Грейт Гейбл',
-      link: 'https://unsplash.com/photos/XyjIUYxJrGo'
-    }
-  ];
+formElement.addEventListener('submit', formSubmitHandler);
